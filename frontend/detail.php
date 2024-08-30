1a3777fda1ace096e603350aad5a9cbc677d85cc
Jeerawat621463007 <jeerawat1977@gmail.com>
2024-08-30 13:36:55 +0700

    dont

---
+++ frontend/detail.php
@@ -0,0 +1,50 @@
+<?php
+require '../connect.php';
+require 'top-nav.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row py-4">
+            <!-------------------- Section Left -------------------->
+            <div class="col-8">
+                <div class="row justify-content-center">
+                    <h4 class="text-center">THIS FOR TITLE</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste dolorum magni sit, odio mollitia, autem molestiae fugiat ad, perspiciatis commodi adipisci deleniti nisi nesciunt incidunt repellendus? Magnam magni quaerat non.</p>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur perferendis sapiente reiciendis. Dignissimos ratione aliquid, tenetur ea architecto in aut perferendis porro dolore facere ex neque quia, minus sunt esse.</p>
+
+                    <!------------------------------------------------------------------- Facilities ------------------------------------------------------------------->
+                    <h4 class="text-center">Facilities</h4>
+
+                    <!------------------------------------------------------------------- Vedio ------------------------------------------------------------------->
+                    <h4 class="text-center">Vedio</h4>
+
+                    <!------------------------------------------------------------------- Booking ------------------------------------------------------------------->
+                    <h4 class="text-center">Booking Policy</h4>
+                    
+                </div>
+            </div>
+
+            <!-------------------- Section Right -------------------->
+            <div class="col-4">
+                <div class="row">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus veniam amet sit officia temporibus consectetur velit. Necessitatibus, sapiente suscipit culpa fugit sed rerum vitae placeat neque dicta totam magni voluptatem?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php
+require 'footer.php';
+?>
